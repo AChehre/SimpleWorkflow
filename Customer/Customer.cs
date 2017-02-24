5def11e8a8a557d4f87ba9645364083183f75cb0
Ahmad <achehre@yahoo.com>
2017-02-24 13:03:59 +0330

    Model added.

---
+++ Customer/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Customer
+{
+    public class Customer
+    {
+        private readonly Guid _CustomerId;
+        public CustomerState State { get; private set; }
+
+        public Customer(Guid customerId)
+        {
+            _CustomerId = customerId;
+            State = CustomerState.Undefined;
+        }
+    }
+}
