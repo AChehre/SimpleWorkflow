aa713ffa6602ef6e81755dab5db2b2bd1beefefa
Ahmad <achehre@yahoo.com>
2017-03-02 11:08:29 +0330

    Tests are added.

---
+++ Tests/CustomerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Domain.Model.Customer;
+using Xunit;
+
+namespace Tests
+{
+    public class CustomerTests
+    {
+
+        [Fact]
+        public void ChangeStateToConfirmed_DocumentsAreSigned_StateChangeToConfirmed()
+        {
+            var customer = new Customer(Guid.NewGuid());
+            customer.SignDocuments();
+
+            customer.ChangeStateToConfirmed();
+            Assert.Equal(customer.State, CustomerState.Confirmed);
+        }
+
+        [Fact]
+        public void ChangeStateToUnconfirmed_DocumentsAreNotSigned_StateChangeToUnconfirmed()
+        {
+            var customer = new Customer(Guid.NewGuid());
+            customer.ChangeStateToUnconfirmed();
+
+            Assert.Equal(customer.State, CustomerState.Unconfirmed);
+        }
+
+      
+    }
+}
